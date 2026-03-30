76f52fcb34072d31ee1e841846c5d8b63915b091
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2026-03-30 22:15:46 +0500

    Prepare for deployment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1", // Backend ka address
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1", // Defaults to localhost for dev
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
